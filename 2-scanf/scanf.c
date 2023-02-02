d59dec0e8d56b64bbefe003ffba80fc332105858
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-02 14:52:38 -0300

    usando laço for

--- 2-scanf/scanf.c
+++ 2-scanf/scanf.c
@@ -4,20 +4,8 @@
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    int a = 9;
-    switch(a)
-    {
-    case 0:
-        printf("numero 0");
-        break;
-    case 1:
-        printf("numero 1");
-        break;
-    case 2:
-        printf("numero 2");
-        break;
-    default:
-        printf("numero fora do range");
+
+    for (int i = 0; i< 10;i++){
+        printf("numero\t%d\n",i);
     }
-    return 0;
 }
