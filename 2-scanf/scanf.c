0adcbd763c34864f1182f3d7dfc45a8fe7c657d1
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-02 15:58:44 -0300

    usando strlen

--- 2-scanf/scanf.c
+++ 2-scanf/scanf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
 #include <locale.h>
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
 
-    int num [2][2] = { {1,2},{1,2} };
+    char nome [3][3] = {{2,4,6},{1,3,5}};
+    int a = strlen(nome);
+    printf("%d",a);
 
-    for(int i = 0;i < 2;i++){
-        for(int j = 0; j < 2; j++){
-            printf("numero[%d][%d] = %d\n",i,j,num[i][j]);
-        }
-    }
 }
