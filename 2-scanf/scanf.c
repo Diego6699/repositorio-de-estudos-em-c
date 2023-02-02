fd86099a6b8100a52d10d53f76c018c493483b24
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-02 15:32:43 -0300

    usando sizeof

--- 2-scanf/scanf.c
+++ 2-scanf/scanf.c
@@ -5,10 +5,10 @@ int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
 
-    char nome[20] = "diego leonardo";
-    int i = 0;
-    while(i < 20){
-        printf("%d\t%c\n",i,nome[i]);
-        i++;
+    char num [] = "diego leonardo da silva frança";
+    int len = sizeof(num)/sizeof(num[0]);
+    for(int i = 0; i < len; i++)
+{
+    printf("%d\t%c\n",i,num[i]);
     }
 }
