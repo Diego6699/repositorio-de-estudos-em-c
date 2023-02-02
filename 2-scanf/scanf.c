404896de65e607196dc26fdacebd8a6b8eef1714
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-02 15:42:12 -0300

    usando matrizes

--- 2-scanf/scanf.c
+++ 2-scanf/scanf.c
@@ -5,10 +5,11 @@ int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
 
-    char num [] = "diego leonardo da silva frança";
-    int len = sizeof(num)/sizeof(num[0]);
-    for(int i = 0; i < len; i++)
-{
-    printf("%d\t%c\n",i,num[i]);
+    int num [2][2] = { {1,2},{1,2} };
+
+    for(int i = 0;i < 2;i++){
+        for(int j = 0; j < 2; j++){
+            printf("numero[%d][%d] = %d\n",i,j,num[i][j]);
+        }
     }
 }
