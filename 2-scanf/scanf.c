e2c1f5f27713e6e6fc32e0b6c19830e2211e04e6
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-02 14:48:32 -0300

    usando swicth ,case , default

--- 2-scanf/scanf.c
+++ 2-scanf/scanf.c
@@ -4,12 +4,20 @@
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    int a = 1;
-    if(a){
-        printf("%d", a);
-        printf("%", a);
-    }else{
-        printf("negativo");
+    int a = 9;
+    switch(a)
+    {
+    case 0:
+        printf("numero 0");
+        break;
+    case 1:
+        printf("numero 1");
+        break;
+    case 2:
+        printf("numero 2");
+        break;
+    default:
+        printf("numero fora do range");
     }
     return 0;
 }
