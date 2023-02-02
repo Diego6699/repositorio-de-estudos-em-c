f179a5f0d2671d1ca6e8f73d0ca7c784b7e33eaa
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-02 14:39:42 -0300

    usando if e else

--- 2-scanf/scanf.c
+++ 2-scanf/scanf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <locale.h>
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    char c;
-    printf("digite o caractere: ");
-    scanf("%c",&c);
-    fgetchar();
-    printf("%c",c);
+    int a = 1;
+    if(a){
+        printf("%d", a);
+        printf("%", a);
+    }else{
+        printf("negativo");
+    }
     return 0;
 }
