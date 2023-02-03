69807d16539d49b9fa81de0e34bdd3d9c9359668
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-03 16:24:37 -0300

    usando ponteiro simples

--- 3-matrizes/matrizes.c
+++ 3-matrizes/matrizes.c
@@ -5,10 +5,8 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    char saudacao [20] = "Ola, ";
-    char saudacao2 [100] = "mundo!";
-    //copiar string
-    strcat(saudacao,saudacao2),
-    printf("%s",saudacao);
+    int num = 123;
+    int *ponteiro = &num;
+    printf("%d", *ponteiro);
     return 0;
 }
