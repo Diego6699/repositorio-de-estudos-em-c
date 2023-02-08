4ec5032cab7d15e079977bb6dd6d4ddf32744397
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-02-08 15:13:26 -0300

    usando strcmp

--- 4-ponteiro/ponteiro.c
+++ 4-ponteiro/ponteiro.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main (void){
-    
+    int p[2] = {1,2};
+    int *pt = &p;
+    printf("%d %d");
     return 0;
 }
---
+++ 6-string/string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = strcmp("dieg", "diego");
+    if (a == 0)
+    {
+        printf("sao iguais: %d",a);
+    }
+    else if (a < 0)
+    {
+        printf("str 1 e menor: %d",a);
+    }
+    else
+    {
+        printf("str 1 e maior: %d",a);
+    }
+
+    return 0;
+}
